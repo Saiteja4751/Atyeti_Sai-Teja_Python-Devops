6cb0cafa3076280445a7daa95cec85a5177d0021
Saiteja4751 <sai.battu@atyeti.com>
2025-06-13 17:53:06 +0530

    Delete Python directory

--- Python/PDBC/Connect.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector as myConn
-mydb=myConn.connect(host='localhost',
-                user='root',
-                password='root'
-                )
-print(mydb,'connection estlablished')
